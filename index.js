223ab5191fb9b897181b52b63d870a9ae07dc9e2
Oleksii Maliarov <lex@debitoor.com>
2017-02-01 10:45:09 +0200

    timeout support

--- index.js
+++ index.js
@@ -1,46 +1,65 @@
-const NoteRateLimiter = require('node-rate-limiter');
-
 const package = require('package.json');
+
 const redis = require('redis');
 const path = require('path');
 const fs = require('fs');
+const once = require('once');
+
+const noteRateLimiter = require('node-rate-limiter');
+
 
 const defaultRateLimit = 5000;
 const defaultExpiration = 1000 * 60 * 60;
+const defaultTimeout = 500;
 
 const script = fs.readFileSync(path.join(__dirname, 'script.lua'), 'utf-8');
 let scriptSha;
 
-NoteRateLimiter.register({name: 'redis', adaptor: RedisAdaptor, ver: package.version});
+noteRateLimiter.registerAdaptor({name: 'redis', adaptor: RedisAdaptor, ver: package.version});
 
 module.exports = RedisAdaptor;
 
 
-function RedisAdaptor(database) {
-    this.prepare = (callback) => prepare(database, callback); 
-    this.reset = (id, callback) => reset(database, id, callback); 
-    this.get = (id, opts, callback) => get(database, id, opts, callback);
+function RedisAdaptor(opts) {
+    opts = opts || {};
+    opts.timeout = opts.timeout || defaultTimeout;
+
+    const adaptorOpts = opts;
+
+    this.prepare = (callback) => prepare(adaptorOpts, callback); 
+    this.reset = (id, callback) => reset(adaptorOpts, id, callback); 
+    this.get = (id, opts, callback) => get(adaptorOpts, id, opts, callback);
 }
 
 
-function reset(db, id, callback) {
+function reset(adaptorOpts, id, callback) {
     const keyClient = 'ratelimiter.' + id;
     const keyClientTotal = keyClient + '.total';
     const keyClientLimit = keyClient + '.limit';
 
-    db
+    const onExecDoneOnce = once(callback);
+
+    adaptorOpts.database
         .multi()
         .del(keyClientTotal)
         .del(keyClientLimit)
         .del(keyClientExpire)
-        .exec((err) => callback(err));
+        .exec(onExecDoneOnce);
+
+    setTimeout(() => onExecDoneOnce(new nodeRateLimiter.TimeoutError()), adaptorOpts.timeout);
 }
 
-function get(db, id, opts, callback) {
+function get(adaptorOpts, id, opts, callback) {
     const limit = opts && opts.limit || defaultRateLimit;
     const expire = opts && opts.expire || defaultExpiration;
 
-    db.evalsha(scriptSha, 3, id, limit, expire, (err, res) => {
+    const onEvalshaDoneOnce = once(onEvalshaDone);
+
+    adaptorOpts.database.evalsha(scriptSha, 3, id, limit, expire, onEvalshaDoneOnce);
+    setTimeout(() => onEvalshaDoneOnce(new nodeRateLimiter.TimeoutError()), adaptorOpts.timeout);
+
+
+    function onEvalshaDone(err, res) {
         if (err) {
             return callback(err);
         }
@@ -50,15 +69,18 @@ function get(db, id, opts, callback) {
             reset: res[2]
         };
         callback(null, result);
-    });
+    }
 }
 
-function prepare(database, callback) {
+function prepare(adaptorOpts, callback) {
     if (scriptSha) {
         return callback(null);
     }
 
-    database.script('load', script, onScriptLoaded);
+    const onScriptLoadedOnce = once(onScriptLoaded);
+
+    adaptorOpts.database.script('load', script, onScriptLoadedOnce);
+    setTimeout(() => onScriptLoadedOnce(new nodeRateLimiter.TimeoutError()), adaptorOpts.timeout);
 
 
     function onScriptLoaded(err, sha) {
